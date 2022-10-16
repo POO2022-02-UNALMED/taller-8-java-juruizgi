4c4d07b10c098bcd180f963d1ff97e239d9a6e55
juruizgi <111092148+juruizgi@users.noreply.github.com>
2022-10-15 19:09:57 -0500

    Update Jugador.java

--- src/main/java/futbol/Jugador.java
+++ src/main/java/futbol/Jugador.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package futbol;
 
-/**
- *
- * @author USUARIO
- */
 public class Jugador extends Futbolista {
     
     public short golesMarcados;
